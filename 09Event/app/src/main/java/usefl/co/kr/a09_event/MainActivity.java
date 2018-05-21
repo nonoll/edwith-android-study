375f3ec1ecd09af9c8a0d8ce93987dddb547a0b3
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-05-21 17:01:59 +0900

    [hkroh] 09Event commit

--- 09Event/app/src/main/java/usefl/co/kr/a09_event/MainActivity.java
+++ 09Event/app/src/main/java/usefl/co/kr/a09_event/MainActivity.java
@@ -2,12 +2,107 @@ package usefl.co.kr.a09_event;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        View view1 = (View) findViewById(R.id.view1);
+
+        view1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("MotionEvent.ACTION_DOWN : " + curX + " , " + curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                    println("MotionEvent.ACTION_MOVE : " + curX + " , " + curY);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    println("MotionEvent.ACTION_UP : " + curX + " , " + curY);
+                }
+
+                return true;
+            }
+        });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println("onDown");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println("onShowPress");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println("onSingleTapUp");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onScroll");
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling");
+                return true;
+            }
+        });
+
+
+        View view2 = (View) findViewById(R.id.view2);
+
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                detector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Toast.makeText(this, "Back Pressed.", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return false;
+//        return super.onKeyDown(keyCode, event);
+    }
+
+    public void println(String data) {
+        textView.append(data + '\n');
     }
 }
