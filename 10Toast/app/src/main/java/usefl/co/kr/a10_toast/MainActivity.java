0751266fb286b40f5c8654059c93582ad6d02cf1
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-05-23 10:56:42 +0900

    [hkroh] 10Toast - custom / snackbar commit

--- 10Toast/app/src/main/java/usefl/co/kr/a10_toast/MainActivity.java
+++ 10Toast/app/src/main/java/usefl/co/kr/a10_toast/MainActivity.java
@@ -1,7 +1,15 @@
 package usefl.co.kr.a10_toast;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "toask message", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP|Gravity.LEFT, 200, 200);
+                toast.show();
+            }
+        });
+
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toastborder, (ViewGroup) findViewById(R.id.toast_layout_root));
+
+                TextView text = layout.findViewById(R.id.text);
+                text.setText("custom toast~");
+
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(layout);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "snackbar~", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
