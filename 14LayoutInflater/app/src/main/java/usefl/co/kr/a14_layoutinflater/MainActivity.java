3f71f899cf9c2f38c898b855460c2e9dd91bc223
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-05-23 17:17:32 +0900

    [hkroh] 14LayoutInflater commit

---
+++ 14LayoutInflater/app/src/main/java/usefl/co/kr/a14_layoutinflater/MainActivity.java
@@ -0,0 +1,36 @@
+package usefl.co.kr.a14_layoutinflater;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = (FrameLayout) findViewById(R.id.container);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+
+                // inflate 후 layout 의 객체 셀렉트
+                TextView textView = (TextView) container.findViewById(R.id.textView);
+                textView.setText("test");
+            }
+        });
+    }
+}
