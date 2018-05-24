115f80d89cebf4aaba3983c4cac304c47c9b8b2b
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-05-24 16:02:36 +0900

    [hkroh] 16Spinner commit

--- 16Spinner/app/src/main/java/usefl/co/kr/a16_spinner/MainActivity.java
+++ 16Spinner/app/src/main/java/usefl/co/kr/a16_spinner/MainActivity.java
@@ -2,12 +2,39 @@ package usefl.co.kr.a16_spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    String[] items = {"item1", "item2", "item3", "item4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                textView.setText(items[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                textView.setText("");
+            }
+        });
     }
 }
