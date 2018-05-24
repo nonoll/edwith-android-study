9b99dc8003d361f741b812767d4ed5d24449cdb4
HyunKwan Roh <hkroh.nonoll@gmail.com>
2018-05-24 11:39:25 +0900

    [hkroh] 15ListView commit

--- 15ListView/app/src/main/java/usefl/co/kr/a15_listview/MainActivity.java
+++ 15ListView/app/src/main/java/usefl/co/kr/a15_listview/MainActivity.java
@@ -1,16 +1,26 @@
 package usefl.co.kr.a15_listview;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    SingerAdapter adapter;
+
+    EditText editTextName;
+    EditText editTextMobile;
+    Button insertButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        SingerAdapter adapter = new SingerAdapter();
+        adapter = new SingerAdapter();
         adapter.addItem(new SingerItem("name1", "010-1234-1234"));
         adapter.addItem(new SingerItem("name2", "010-1234-1234"));
         adapter.addItem(new SingerItem("name3", "010-1234-1234"));
@@ -31,6 +41,33 @@ public class MainActivity extends AppCompatActivity {
         adapter.addItem(new SingerItem("name9", "010-1234-1234"));
         adapter.addItem(new SingerItem("name10", "010-1234-1234"));
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SingerItem item = (SingerItem) adapter.getItem(i);
+
+                // Toast.makeText(getApplicationContext(), item.name, Toast.LENGTH_LONG).show();
+                Snackbar.make(view, "name : " + item.name + " / mobile : " + item.mobile, Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextMobile = (EditText) findViewById(R.id.editTextMobile);
+        insertButton = (Button) findViewById(R.id.insertButton);
+
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString();
+                String mobile = editTextMobile.getText().toString();
+
+                adapter.addItem(new SingerItem(name, mobile));
+                adapter.notifyDataSetChanged();
+
+                Toast.makeText(getApplicationContext(), "added", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
@@ -58,7 +95,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            SingerItemView itemView = new SingerItemView(getApplicationContext());
+            SingerItemView itemView = null;
+
+            // 최적화를 위한 처리
+            if (view == null) {
+                itemView = new SingerItemView(getApplicationContext());
+            } else {
+                itemView = (SingerItemView) view;
+            }
+
             SingerItem item = items.get(i);
 
             itemView.setName(item.getName());
